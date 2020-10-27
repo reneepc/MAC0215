8956f7b585bab224639f23cec9dfce6339719e2f
Renê Cardozo <rene.cardozo@usp.br>
2020-10-27 14:01:32 -0300

    Gráficos e animação

---
+++ graficos/script-graficos.sh
@@ -0,0 +1,5 @@
+python3 analyse.py wifi-test-result/ "TxPower = 16 dBm | Modelo Estático" "tx-16-dbm-estatico" > graficos/tx-16-dbm-estatico.txt
+
+python3 analyse.py wifi-test-random-result/ "TxPower = 16 dBm" "tx-16-dbm" > graficos/tx-16-dbm.txt
+
+python3 analyse.py wifi-greater-tx-power-result/ "TxPower = 36 dBm" "tx-36-dbm" > graficos/tx-36-dbm.txt
